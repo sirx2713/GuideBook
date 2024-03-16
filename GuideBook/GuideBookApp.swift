349f2bbe2320e892242f45a6d79e1a6f19014d37
sirx2713 <bradx2713@gmail.com>
2024-03-16 22:47:22 +0300

    Initial Commit

---
+++ GuideBook/GuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBookApp.swift
+//  GuideBook
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/16.
+//
+
+import SwiftUI
+
+@main
+struct GuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
